c05d2d612503f572e99c71ac4b2c81b20acd4996
Ty Seddon <ty.seddon@cognivista.com>
2022-02-11 20:35:02 -0500

    joi client

--- joiclient.py
+++ joiclient.py
@@ -28,56 +28,73 @@ class JoiClient():
         """ Create a JoiClient for the given device_id.  Get the device_id from enviro.yaml"""
         self.device_id = device_id
         device = self._get_Device(self.device_id)
-        self.resident_id = device.resident_id
+        self.resident_id = device.resident
         self.token = self._login(self.resident_id)
 
     def _login(self, resident_id):
-        response = requests.post(LOGIN_PATH, 
-                        data=str({
-                            'username': enviro.get_value('username', resident_id),
-                            'password': enviro.get_value('password', resident_id),
-                        }))
-        return munchify(json.loads(response.content)).token
+        username = enviro.get_value('username', resident_id)
+        password = enviro.get_value('password', resident_id)
+        response = requests.post(LOGIN_PATH,
+                        json={
+                            'username': username,
+                            'password': password
+                        })
+        if response.status_code == 200:
+            return munchify(json.loads(response.content)).token
+        else:
+            raise Exception(f"Error calling {response.url} Status code {response.status_code}.  {response.reason} {response.content}")     
 
     def _build_header(self):
-        return {'Authorization': 'Bearer {}'.format(self.token), 'Content-Type': 'application/json'}
+        return {'Authorization': 'Token {}'.format(self.token), 'Content-Type': 'application/json'}
 
     def _get_Device(self, device_id):
         response = requests.get(f"{DEVICE_PATH}{device_id}") # no credentials required for this call
         return munchify(json.loads(response.content)) 
 
     def get_Resident(self):
-        response = requests.get(f"{RESIDENT_PATH}{self.resident_id}", header=self._build_header())
-        return munchify(json.loads(response.content))
+        response = requests.get(f"{RESIDENT_PATH}{self.resident_id}", headers=self._build_header())
+        if response.status_code == 200:
+            return munchify(json.loads(response.content))
+        else:
+            raise Exception(f"Error calling {response.url} Status code {response.status_code}.  {response.reason} {response.content}")     
 
     def list_MemoryBoxes(self):
-        response = requests.get(f"{MEMORYBOX_PATH}", header=self._build_header())
-        return munchify(json.loads(response.content))
+        response = requests.get(f"{MEMORYBOX_PATH}", headers=self._build_header())
+        if response.status_code == 200:
+            return munchify(json.loads(response.content))
+        else:
+            raise Exception(f"Error calling {response.url} Status code {response.status_code}.  {response.reason} {response.content}")     
 
     def start_MemoryBoxSession(self, memorybox_id, start_method):
         response = requests.post(MEMORYBOXSESSION_PATH, headers=self._build_header(), 
-                    data=str({
+                    json={
                         'memorybox_id': memorybox_id,
                         'resident_id' : self.resident_id,
                         'device_id': self.device_id,
                         'session_start_method': start_method,
-                        'session_start_datetime': datetime.utcnow(),
-                    }))
-        return munchify(json.loads(response.content)) 
+                        'session_start_datetime': datetime.utcnow()
+                    })
+        if response.status_code == 200:
+            return munchify(json.loads(response.content))
+        else:
+            raise Exception(f"Error calling {response.url} Status code {response.status_code}.  {response.reason} {response.content}")     
 
     def end_MemoryBoxSession(self, memorybox_session_id, session_end_method, resident_self_reported_feeling):
         response = requests.post(MEMORYBOXSESSION_PATH, headers=self._build_header(), 
-                    data=str({
+                    json={
                         'memorybox_session_id': memorybox_session_id,
                         'session_end_method': session_end_method,
                         'session_end_datetime': datetime.utcnow(),
-                        'resident_self_reported_feeling': resident_self_reported_feeling,
-                    }))
-        return munchify(json.loads(response.content)) 
+                        'resident_self_reported_feeling': resident_self_reported_feeling
+                    })
+        if response.status_code == 200:
+            return munchify(json.loads(response.content))
+        else:
+            raise Exception(f"Error calling {response.url} Status code {response.status_code}.  {response.reason} {response.content}")     
 
     def start_MemoryBoxSessionMedia(self, memorybox_session_id, media_url, media_name, media_artist, media_tags, media_classification):
         response = requests.post(MEMORYBOXSESSIONMEDIA_PATH, headers=self._build_header(), 
-                    data=str({
+                    json={
                         'memorybox_session_id': memorybox_session_id,
                         'resident_id' : self.resident_id,
                         'media_url': media_url,
@@ -86,29 +103,38 @@ class JoiClient():
                         'media_artist': media_artist,
                         'media_tags': media_tags,
                         'media_classification': media_classification
-                    }))
-        return munchify(json.loads(response.content)) 
+                    })
+        if response.status_code == 200:
+            return munchify(json.loads(response.content))
+        else:
+            raise Exception(f"Error calling {response.url} Status code {response.status_code}.  {response.reason} {response.content}")     
 
     def end_MemoryBoxSessionMedia(self, memorbybox_session_media_id, resident_motion, resident_utterances, resident_self_reported_feeling):
         response = requests.patch(MEMORYBOXSESSIONMEDIA_PATH, headers=self._build_header(), 
-                    data=str({
+                    json={
                         'memorbybox_session_media_id': memorbybox_session_media_id,
                         'media_end_datetime': datetime.utcnow(),
                         'resident_motion': resident_motion,
                         'resident_utterances': resident_utterances,
                         'resident_self_reported_feeling': resident_self_reported_feeling
-                    }))
-        return munchify(json.loads(response.content)) 
+                    })
+        if response.status_code == 200:
+            return munchify(json.loads(response.content))
+        else:
+            raise Exception(f"Error calling {response.url} Status code {response.status_code}.  {response.reason} {response.content}")     
 
     def add_MediaInteraction(self, memorybox_session_media, media_percent_completed, event, data):
         response = requests.post(MEDIAINTERACTION_PATH, headers=self._build_header(), 
-                    data=str({
+                    json={
                         'memorybox_session_media': memorybox_session_media,
                         'resident_id' : self.resident_id,
                         'log_datetime': datetime.utcnow(),
                         'media_percent_completed': media_percent_completed,
                         'event': event,
                         'data': data,
-                    }))
-        return munchify(json.loads(response.content)) 
+                    })
+        if response.status_code == 200:
+            return munchify(json.loads(response.content))
+        else:
+            raise Exception(f"Error calling {response.url} Status code {response.status_code}.  {response.reason} {response.content}")     
 
--- test_joiclient.py
+++ test_joiclient.py
@@ -6,3 +6,6 @@ client = JoiClient(device_id)
 resident = client.get_Resident()
 print(resident)
 
+memoryboxes = client.list_MemoryBoxes()
+print(memoryboxes)
+
